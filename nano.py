f0a32c48a773e504bdd05602f9f834944e6819bd
Jonas Haag <jonas@lophus.org>
2010-12-24 16:29:43 +0100

    Refactor

--- nano.py
+++ nano.py
@@ -17,22 +17,16 @@ def format_status(status):
         status = '%d %s' % (status, HTTP_REASONS.get(status, '<reason>'))
     return status
 
-class HeaderDict(dict):
-    def __setitem__(self, key, value):
-        dict.__setitem__(self, key.title(), str(value))
-
-    def setdefault(self, key, value):
-        return dict.setdefault(self, key.title(), str(value))
+def isetdefault(dct, key, value):
+    dct.setdefault(key.title(), str(value))
 
 class HttpError(Exception):
     def __init__(self, status, *args):
-        Exception.__init__(self, format_status(status), *args)
-
-class Http500(HttpError):
-    def __init__(self, *args):
-        HttpError.__init__(self, *((500,)+args))
+        Exception.__init__(self, status, *args)
 
 class NanoApplication(object):
+    charset = 'utf-8'
+
     def __init__(self, debug=False):
         self.routes = []
         self.debug = debug
@@ -49,59 +43,55 @@ class NanoApplication(object):
         callback, kwargs = self.dispatch(environ)
 
         if callback is None:
-            start_response(format_status(404), [])
+            start_response(format_status(404), [('Content-Length', 0)])
             return []
-
-        response = None
-        body = None
-        status = format_status(200)
-        headers = HeaderDict()
 
         try:
             try:
-                response = callback(environ, **kwargs)
-            except HttpError:
-                raise
+                retval = callback(environ, **kwargs)
             except Exception, e:
-                raise HttpError, HttpError('500 OH NOEZ', str(e)), sys.exc_info()[2]
+                if isinstance(e, HttpError):
+                    raise
+                raise HttpError, HttpError(500, sys.exc_info())
         except HttpError, e:
             status = e.args[0]
-            if app.debug:
-                traceback.print_exc()
-                body = traceback.format_exc()
+            headers = {}
+            exc_info = e.args[1] if len(e.args) > 1 else sys.exc_info()
+            body = traceback.format_exception(*exc_info) if self.debug else ''
+            traceback.print_exception(*exc_info)
+        else:
+            if isinstance(retval, tuple) and len(retval) == 3:
+                status, headers, body = retval
+                headers = dict(headers)
             else:
+                status, headers, body = 200, {}, retval
+
+        if isinstance(body, (list, tuple)):
+            if not body:
                 body = ''
-
-        if response is not None:
-            if isinstance(response, tuple) and len(response) == 3:
-                status, body, headers = response
-            elif isinstance(response, list) and len(response) == 1:
-                body = response[0]
             else:
-                body = response
+                assert isinstance(body[0], (bytes, unicode))
+                body = body[0][0:0].join(body)
 
-        headers = HeaderDict(headers)
+        if isinstance(body, (bytes, unicode)):
+            if isinstance(body, unicode):
+                body = body.encode(self.charset)
+            isetdefault(headers, 'Content-Length', len(body))
+            isetdefault(headers, 'Content-Type', 'text/plain')
+            body = [body] if body else []
+        elif isinstance(body, file):
+            isetdefault(headers, 'Content-Length', os.path.getsize(body.name))
+            mime, _ = mimetypes.guess_type(body.name)
+            if mime is not None:
+                isetdefault(headers, 'Content-Type', mime)
 
-        if isinstance(body, (bytes, str)):
-            headers.setdefault('Content-Length', len(body))
-            headers.setdefault('Content-Type', 'text/plain')
-        elif hasattr(body, 'read') and hasattr(body, 'name'):
-            try:
-                headers.setdefault('Content-Length', os.path.getsize(body.name))
-                mime, _ = mimetypes.guess_type(body.name)
-                if mime is not None:
-                    headers.setdefault('Content-Type', mime)
-            except TypeError:
-                pass
-
+        if isinstance(status, int):
+            status = format_status(status)
         start_response(status, headers.items())
         return body
 
     def dispatch(self, environ):
         request_path = environ['PATH_INFO'] or '/'
-        if 'QUERY_STRING' in environ:
-            request_path += '?' + environ['QUERY_STRING']
-
         for route, callback in self.routes:
             match = route.match(request_path)
             if match is not None:
